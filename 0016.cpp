6551f506e17cbb7dc6f62f7112234a7fbd9db1ad
Zu-rin <54212697+Zu-rin@users.noreply.github.com>
2020-04-30 17:10:12 +0900

    Create 0016.cpp

---
+++ 0016.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <queue>
+#include <algorithm>
+#define rep(i, n) for(i = 0; i < (n); i++)
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define MOD 1000003
+#define PI 3.14159265358979323846
+#define INF 1 << 30
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pp;
+
+ll Pow(ll n, ll k) {
+	ll ans = 1, a = n % MOD;
+	while (k > 0) {
+		if (k & 1) {
+			ans *= a;
+			ans %= MOD;
+		}
+		a *= a;
+		a %= MOD;
+		k >>= 1;
+	}
+	return ans;
+}
+
+int main(void) {
+	int num, i, k;
+	ll a, ans;
+	cin >> a >> num;
+	rep(i, num) {
+		cin >> k;
+		ans += Pow(a, k);
+		ans %= MOD;
+	}
+	cout << ans << "\n";
+	return 0;
+}
